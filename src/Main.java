fbf468c891784fd1a04f0368b27e822b5674bab7
Bobizl <bobi_aytos@abv.bg>
2023-05-19 14:36:46 +0300

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        String file = "src\\WeatherReport.csv";
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String line = "";
+        try{
+            while((line = reader.readLine()) != null){
+
+                String[] row = line.split(";");
+                for(String index : row){
+                    System.out.printf("%-20s", index);
+                }
+                System.out.println();
+            }
+        }catch(Exception ex ){
+            ex.printStackTrace();
+        }finally {
+            reader.close();
+        }
     }
 }
